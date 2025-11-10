20991ac8779fad5decafa88a17b65aef5564f040
Joseph Marube <j.marube@alustudent.com>
2025-11-10 18:09:20 +0200

    quick fix

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-def multiple_returns(sentence):
-    length = len(sentence)
-    first_char = sentence[0] if length > 0 else "None"
-    tup = length, first_char
-    return tup
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return ("None")
+    y = my_list[0]
+    for i in my_list:
+        if i > y:
+            y = i
+    return (y)
