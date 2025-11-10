0c95ee20111e8e4d3c93eeb42f579b4519a88e3d
Joseph Marube <j.marube@alustudent.com>
2025-11-10 18:05:58 +0200

    py style fix

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,5 +2,5 @@
 def delete_at(my_list=[], idx=0):
     """Delete an item at a specific position in a list."""
     if 0 <= idx < len(my_list):
-        del(my_list[idx])
-    return(my_list)
+        del my_list[idx]
+    return my_list
