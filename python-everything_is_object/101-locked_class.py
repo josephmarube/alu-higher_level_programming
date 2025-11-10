2a6c5d2f35e989e4e3f44a26da89b5474a3ec803
Joseph Marube <j.marube@alustudent.com>
2025-11-10 23:27:59 +0200

    class edit

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -3,6 +3,12 @@
 
 
 class LockedClass:
+    """A class that prevents the creation of new attributes other than 'first_name'."""
 
+    __slots__ = ["first_name"]  # Only allow 'first_name' as an attribute
 
-    __slots__ = ["first_name"]
+    def __setattr__(self, name, value):
+        # Prevent setting any attribute other than 'first_name'
+        if name != "first_name":
+            raise AttributeError(f"'LockedClass' object has no attribute '{name}'")
+        super().__setattr__(name, value)
